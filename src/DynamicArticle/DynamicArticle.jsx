a8153694d1c86d971c0b1f477602d276d699d8c7
zhong.liu <zhong.liu@exxactcorp.com>
2020-10-10 11:08:48 -0700

    homework

--- src/DynamicArticle/DynamicArticle.jsx
+++ src/DynamicArticle/DynamicArticle.jsx
@@ -2,11 +2,20 @@ import React from "react";
 import HTMLText from "../HTMLText/HTMLText";
 
 const DynamicArticle = (props) => {
+  const article = props.article;
   return (
     <article>
       {/* this is an example of how you insert props into a react component */}
-      <div>{props.article.title}</div>
-      <HTMLText text={props.article.text} />
+      <header>
+        <h1>{article.title}</h1>
+        <address>
+          by {article.author} (
+          <a href={article.authorEmail}>{article.authorEmail}</a>)
+          <br />
+        </address>
+        <time dateTime={article.timeStamp}>{article.displayDate}</time>
+      </header>
+      <HTMLText text={article.text} />
     </article>
   );
 };
